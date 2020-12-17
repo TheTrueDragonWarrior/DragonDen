611ee25aee83cd7bae7699b09ad4390b2bde4cf5
Jbailey2025 <75849256+Jbailey2025@users.noreply.github.com>
2020-12-17 09:49:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const Discord = require ('discord.js')
 
+const client = new Discord.Client ();
+
+client.once ('ready', ()=> {
+
+    console.log ('ready!');
+
+});
+
+client.login ('Nzg4ODg0OTk2OTc4NzY5OTQw.X9qAaA.Y9DyQFxydBbnIZLLUQAxmgq49tg')
