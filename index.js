4addff199ab9812972a98e646f43a21589c76860
Jbailey2025 <75849256+Jbailey2025@users.noreply.github.com>
2020-12-18 19:53:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const Discord = require ('discord.js')
 
 const client = new Discord.Client ();
 
-client.once ('ready', ()=> {
+client.on ('ready', ()=> {
 
-    console.log ('ready!');
+  var generalChannel = client.channels.get ("786410076863856647")
+    generalChannel.send("Hello,world!")
 
-});
 
+    console.log("connected as " + client.user.tag)
+
+    client.user.setActivity("YouTube", {type:"WATCHING"})
+    client.guilds.forEach((guild) => {
+        console.log(guild.name)
+        guild.channels.forEach((channel) => {
+            console.log (" - " + guild.name)
+
+        guild.channels.forEach((channel) =>
+             console.log (`-- ${channel.name} ({channel.type} - ${channel.id})`)
+        })
+    })
+})
+
+var generalChannel = client.channels.get ("786410076863856647")
+const attachment = new Discord.Attachment(https://tzaphiel.files.wordpress.com/2013/01/fire-breathing-dragon-2560x1600.jpg)
+    generalChannel.send(attachment)
+
+client.login ('Nzg4ODg0OTk2OTc4NzY5OTQw.X9qAaA.ISRuH0LCTqgE4PSIJYf8RAtFAo0')
+client.on ('message', message => {
+    console.log(message.content);
+}))
+
+const PREFIX = 'dd'  
 client.login ('xxxxxx')
