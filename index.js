318855d66da90c0a00cdf52dd67901f2695a5170
Jbailey2025 <75849256+Jbailey2025@users.noreply.github.com>
2020-12-19 10:12:23 -0500

    Update index.js
    Finished beginning product maybe

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-const Discord = require ('discord.js')
+const Discord = require('discord.js')
 
-const client = new Discord.Client ();
+const client = new Discord.Client();
 
 client.on ('ready', ()=> {
-
-  var generalChannel = client.channels.get ("786410076863856647")
-    generalChannel.send("Hello,world!")
-
-
-    console.log("connected as " + client.user.tag)
-
-    client.user.setActivity("YouTube", {type:"WATCHING"})
-    client.guilds.forEach((guild) => {
-        console.log(guild.name)
-        guild.channels.forEach((channel) => {
-            console.log (" - " + guild.name)
-
-        guild.channels.forEach((channel) =>
-             console.log (`-- ${channel.name} ({channel.type} - ${channel.id})`)
-        })
+  console.log("connected as " + client.user.tag)
+  const generalChannel = client.channels.cache.get("786410076863856647")
+  
+  if(generalChannel){
+    generalChannel.send("Wazzzzuuupppp")
+    const attachment = "https://tzaphiel.files.wordpress.com/2013/01/fire-breathing-dragon-2560x1600.jpg"
+    generalChannel.send("sending an image", {files:[attachment]})
+  }else{
+    console.log("the channel id was invalid")
+  }
+  client.user.setActivity("One Server", {type:"WATCHING"})
+  client.guilds.cache.forEach((guild) => {
+    console.log(guild.name)
+    guild.channels.cache.forEach((channel) => {
+      console.log (" - " + guild.name)
+      guild.channels.cache.forEach((channel) =>{
+            console.log (`-- ${channel.name} ({channel.type} - ${channel.id})`)
+      })
     })
+  })
 })
 
-var generalChannel = client.channels.get ("786410076863856647")
-const attachment = new Discord.Attachment(https://tzaphiel.files.wordpress.com/2013/01/fire-breathing-dragon-2560x1600.jpg)
-    generalChannel.send(attachment)
+// var generalChannel = client.channels.get("786410076863856647")
 
 
 client.on ('message', message => {
     console.log(message.content);
-}))
+})
 
-const PREFIX = 'dd'  
-client.login ('xxxxxx')
+const PREFIX = 'dd'
+client.login(process.env.DISCORD_BOT_SECRET)
